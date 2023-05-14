b0ab4289da70ea3c9bed00484ba0b352c2e954ad
TechPizza <23627133+TechnologicalPizza@users.noreply.github.com>
2023-05-14 02:35:32 +0200

    Fixed AC_BlockColor.getColorMultiplier

--- src/main/java/dev/adventurecraft/awakening/common/AC_BlockColor.java
+++ src/main/java/dev/adventurecraft/awakening/common/AC_BlockColor.java
@@ -2,10 +2,32 @@ package dev.adventurecraft.awakening.common;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.BlockView;
 
 public class AC_BlockColor extends Block implements AC_IBlockColor {
 
     public AC_BlockColor(int var1, int var2, Material var3) {
         super(var1, var2, var3);
+    }
+
+    @Override
+    public int getColorMultiplier(BlockView view, int x, int y, int z) {
+        int meta = this.getColorMetaData(view, x, y, z);
+        if (meta == 1) {
+            meta = 16775065;
+        } else if (meta == 2) {
+            meta = 16767663;
+        } else if (meta == 3) {
+            meta = 10736540;
+        } else if (meta == 4) {
+            meta = 9755639;
+        } else if (meta == 5) {
+            meta = 8880573;
+        } else if (meta == 6) {
+            meta = 15539236;
+        } else {
+            meta = defaultColor;
+        }
+        return meta;
     }
 }
--- src/main/java/dev/adventurecraft/awakening/common/AC_IBlockColor.java
+++ src/main/java/dev/adventurecraft/awakening/common/AC_IBlockColor.java
@@ -8,26 +8,6 @@ public interface AC_IBlockColor {
     int numColors = 7;
     int defaultColor = 13421772;
 
-    default int getColorMultiplier(BlockView view, int x, int y, int z) {
-        int meta = this.getColorMetaData(view, x, y, z);
-        if (meta == 1) {
-            meta = 16775065;
-        } else if (meta == 2) {
-            meta = 16767663;
-        } else if (meta == 3) {
-            meta = 10736540;
-        } else if (meta == 4) {
-            meta = 9755639;
-        } else if (meta == 5) {
-            meta = 8880573;
-        } else if (meta == 6) {
-            meta = 15539236;
-        } else {
-            meta = defaultColor;
-        }
-        return meta;
-    }
-
     default int getColorMetaData(BlockView view, int x, int y, int z) {
         return view.getBlockMeta(x, y, z);
     }
