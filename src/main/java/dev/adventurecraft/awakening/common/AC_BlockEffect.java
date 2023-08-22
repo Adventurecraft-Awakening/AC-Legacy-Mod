Adventurecraft-Awakening/AC-Legacy-Mod

commit 20fc7e85df4fdcc0ef72eb1618e9c49b70ef1baa

Small script loading cleanup

src/main/java/dev/adventurecraft/awakening/common/AC_JScriptHandler.java
@@ -12,7 +12,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class AC_JScriptHandler {
 
@@ -26,14 +29,33 @@ public class AC_JScriptHandler {
         this.scripts = new HashMap<>();
     }
 
+    public Stream<Path> getFiles() throws IOException {
+        //noinspection resource
+        return Files.walk(this.scriptDir.toPath(), 1).filter(Files::isRegularFile);
+    }
+
+    public String[] getFileNames() {
+        try {
+            return getFiles().map(path -> path.getFileName().toString()).toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void loadScripts(ProgressListener progressListener) {
         this.scripts.clear();
 
         if (progressListener != null)
             progressListener.notifyIgnoreGameRunning("Loading scripts");
 
-        File[] files = this.scriptDir.listFiles();
-        if (files == null) {
+        File[] files;
+        try {
+            files = this.getFiles().map(Path::toFile).toArray(File[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (files.length == 0) {
             return;
         }
 

src/main/java/dev/adventurecraft/awakening/common/AC_GuiMobSpawner.java
@@ -1,12 +1,16 @@
 package dev.adventurecraft.awakening.common;
 
+import dev.adventurecraft.awakening.extension.client.render.ExTextRenderer;
 import dev.adventurecraft.awakening.extension.world.ExWorld;
 import dev.adventurecraft.awakening.script.EntityDescriptions;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.render.Tessellator;
 import net.minecraft.item.Item;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -239,7 +243,7 @@ public class AC_GuiMobSpawner extends Screen {
     }
 
     @Override
-    public void render(int mouseX, int mouseY, float deltaTime) {
+    public void render(int mouseX, int mouseY, float tickTime) {
         this.fill(0, 0, this.width, this.height, Integer.MIN_VALUE);
         this.drawTextWithShadow(this.textRenderer, String.format("Entity Spawn: %s", this.mobSpawner.entityID), 4, 4, 14737632);
         this.drawTextWithShadow(this.textRenderer, String.format("Entities Alive: %d", this.mobSpawner.getNumAlive()), 4, 14, 14737632);
@@ -254,7 +258,7 @@ public class AC_GuiMobSpawner extends Screen {
         this.spawnCountSlider.text = String.format("Spawn Count: %d", this.mobSpawner.spawnNumber);
         this.mobSpawner.respawnDelay = (int) (this.respawnSlider.sliderValue * 12000.0F + 0.5F);
         this.respawnSlider.text = String.format("Respawn Delay: %.1fs", (float) this.mobSpawner.respawnDelay / 20.0F);
-        super.render(mouseX, mouseY, deltaTime);
+        super.render(mouseX, mouseY, tickTime);
         this.mobSpawner.world.getChunk(this.mobSpawner.x, this.mobSpawner.z).method_885();
     }
 

src/main/java/dev/adventurecraft/awakening/mixin/world/MixinWorld.java
@@ -1589,17 +1589,7 @@ public abstract class MixinWorld implements ExWorld, BlockView {
 
     @Override
     public String[] getScriptFiles() {
-        File scriptDir = new File(this.levelDir, "scripts");
-        File[] files = scriptDir.listFiles();
-        if (files == null) {
-            return null;
-        }
-
-        String[] scripts = new String[files.length];
-        for (int i = 0; i < scripts.length; i++) {
-            scripts[i] = files[i].getName();
-        }
-        return scripts;
+        return this.scriptHandler.getFileNames();
     }
 
     @Override

commit d9f4972b319683825a38306f8a0bea8d6706aa45

Remove exception introduced in https://github.com/Adventurecraft-Awakening/AC-Legacy-Mod/commit/20fc7e85df4fdcc0ef72eb1618e9c49b70ef1baa

src/main/java/dev/adventurecraft/awakening/common/AC_BlockEffect.java
@@ -118,7 +118,7 @@ public class AC_BlockEffect extends BlockWithEntity implements AC_ITriggerBlock 
         boolean replaced = false;
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(texFile));
+            var reader = new BufferedReader(new FileReader(texFile));
             try {
                 while (reader.ready()) {
                     String line = reader.readLine();

src/main/java/dev/adventurecraft/awakening/common/AC_JScriptHandler.java
@@ -38,7 +38,7 @@ public class AC_JScriptHandler {
         try {
             return getFiles().map(path -> path.getFileName().toString()).toArray(String[]::new);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return null;
         }
     }
 
@@ -52,7 +52,7 @@ public class AC_JScriptHandler {
         try {
             files = this.getFiles().map(Path::toFile).toArray(File[]::new);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return;
         }
 
         if (files.length == 0) {