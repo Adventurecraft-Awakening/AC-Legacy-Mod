ddb61e960b1690ad690eb6bc824342dccbdc6756
Hormez2 <jonas.hormess@web.de>
2024-06-24 21:16:04 +0200

    - Changed the Cuboid Hashmap to a LinkedList, because the usage of the map is not used anywhere in AC + Added the possibility to recolor the models or set a brightness to the model + Models now can use a "mode" to choose if you want to use the lightning or not Modes: - 0 or below: default shadow (can cause black colored models) - 1: is like 0 but it uses the attached entity position - 2 or above: the scripter can set a color or change the brightness This option give the scripter more possibilites how to handle the models

--- src/main/java/dev/adventurecraft/awakening/script/ScriptModel.java
+++ src/main/java/dev/adventurecraft/awakening/script/ScriptModel.java
@@ -1,7 +1,6 @@
 package dev.adventurecraft.awakening.script;
 
 import dev.adventurecraft.awakening.extension.client.model.ExCuboid;
-import dev.adventurecraft.awakening.extension.world.ExWorld;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.Cuboid;
 import net.minecraft.client.texture.TextureManager;
@@ -13,13 +12,13 @@ import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector4f;
 
 import java.nio.FloatBuffer;
-import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class ScriptModel {
 
-    private final HashMap<String, Cuboid> boxes = new HashMap<>();
+    private final List<Cuboid> boxes = new LinkedList<>();
     public ScriptEntity attachedTo;
     public ScriptModel modelAttachment;
     public String texture;
@@ -35,14 +34,16 @@ public class ScriptModel {
     public float yaw;
     public float pitch;
     public float roll;
-
-    public boolean disableLightning = false;
+    public int modes = 0;
+    public float colorRed = 1.0F;
+    public float colorGreen = 1.0F;
+    public float colorBlue = 1.0F;
     private int textureWidth = 64;
     private int textureHeight = 32;
     private static final FloatBuffer modelView = BufferUtils.createFloatBuffer(16);
-    private static Matrix4f transform = new Matrix4f();
-    private static Vector4f v = new Vector4f();
-    private static Vector4f vr = new Vector4f();
+    private static final Matrix4f transform = new Matrix4f();
+    private static final Vector4f v = new Vector4f();
+    private static final Vector4f vr = new Vector4f();
     private static final LinkedList<ScriptModel> activeModels = new LinkedList<>();
 
     public ScriptModel() {
@@ -54,16 +55,26 @@ public class ScriptModel {
         this.textureHeight = height;
     }
 
+    public void setBrightness(float brightness){
+        this.colorRed = this.colorGreen = this.colorBlue = brightness;
+    }
+
     public void addBox(String boxName, float var2, float var3, float var4, int var5, int var6, int var7, int var8, int var9) {
-        this.addBoxExpanded(boxName, var2, var3, var4, var5, var6, var7, var8, var9, 0.0F);
+        this.addBoxExpanded(var2, var3, var4, var5, var6, var7, var8, var9, 0.0F);
+    }
+    public void addBox(float var2, float var3, float var4, int var5, int var6, int var7, int var8, int var9) {
+        this.addBoxExpanded(var2, var3, var4, var5, var6, var7, var8, var9, 0.0F);
     }
 
     public void addBoxExpanded(String boxName, float var2, float var3, float var4, int var5, int var6, int var7, int var8, int var9, float var10) {
+        this.addBoxExpanded(var2, var3, var4, var5, var6, var7, var8, var9, 0.0F);
+    }
+    public void addBoxExpanded(float var2, float var3, float var4, int var5, int var6, int var7, int var8, int var9, float var10) {
         Cuboid cuboid = new Cuboid(var8, var9);
         ((ExCuboid) cuboid).setTWidth(this.textureWidth);
         ((ExCuboid) cuboid).setTHeight(this.textureHeight);
         ((ExCuboid) cuboid).addBoxInverted(var2, var3, var4, var5, var6, var7, var10);
-        this.boxes.put(boxName, cuboid);
+        this.boxes.add(cuboid);
     }
 
     public void setPosition(double x, double y, double z) {
@@ -132,15 +143,14 @@ public class ScriptModel {
         } else if (this.modelAttachment != null) {
             this.modelAttachment.transform(deltaTime);
         }
-
-        float var9 = 1.0F - deltaTime;
-        double var10 = (double) deltaTime * this.x + (double) var9 * this.prevX;
-        double var5 = (double) deltaTime * this.y + (double) var9 * this.prevY;
-        double var7 = (double) deltaTime * this.z + (double) var9 * this.prevZ;
-        GL11.glTranslated(var10, var5, var7);
-        GL11.glRotatef(deltaTime * this.yaw + var9 * this.prevYaw, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(deltaTime * this.pitch + var9 * this.prevPitch, 1.0F, 0.0F, 0.0F);
-        GL11.glRotatef(deltaTime * this.roll + var9 * this.prevRoll, 0.0F, 0.0F, 1.0F);
+        float deltaTimeTick = 1.0F - deltaTime;
+        double x = (double) deltaTime * this.x + (double) deltaTimeTick * this.prevX;
+        double y = (double) deltaTime * this.y + (double) deltaTimeTick * this.prevY;
+        double z = (double) deltaTime * this.z + (double) deltaTimeTick * this.prevZ;
+        GL11.glTranslated(x, y, z);
+        GL11.glRotatef(deltaTime * this.yaw + deltaTimeTick * this.prevYaw, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(deltaTime * this.pitch + deltaTimeTick * this.prevPitch, 1.0F, 0.0F, 0.0F);
+        GL11.glRotatef(deltaTime * this.roll + deltaTimeTick * this.prevRoll, 0.0F, 0.0F, 1.0F);
     }
 
     private void render(float var1) {
@@ -161,21 +171,19 @@ public class ScriptModel {
         v.set(0.0F, 0.0F, 0.0F, 1.0F);
         Matrix4f.transform(transform, v, vr);
 
-        float brightness = 1.0F;
-        //a provisional/alternative fix for the "model shadow"
-        //it only occurs because the coordination of the model is getting rounded and most likely its the position next to it
-        if(attachedTo != null && !disableLightning){
-            var position = attachedTo.getPosition();
-            int eX = (int)Math.floor(position.x);
-            int eY = (int)Math.floor(position.y);
-            int eZ = (int)Math.floor(position.z);
-            brightness = world.method_1782(eX,eY,eZ);
+        if (modes >= 1) {
+            if(modes == 1) {
+                var position = attachedTo.getPosition();
+                setBrightness(world.method_1782((int) Math.floor(position.x), (int) Math.floor(position.y), (int) Math.floor(position.z)));
+            }
+        } else {
+            setBrightness(world.method_1782(Math.round(vr.x), Math.round(vr.x), Math.round(vr.x)));
         }
-        GL11.glColor3f(brightness, brightness, brightness);
+        GL11.glColor3f(colorRed, colorGreen, colorBlue);
         GL11.glPushMatrix();
         this.transform(var1);
 
-        for (Cuboid cuboid : this.boxes.values()) {
+        for (Cuboid cuboid : this.boxes) {
             cuboid.render(1.0F / 16.0F);
         }
 
