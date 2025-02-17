6c1124901e0b6e4fb06796b7a31c3e5f55a27feb
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-02-17 22:09:18 +0100

    Use fast random in LiquidTile fizz

--- src/main/java/dev/adventurecraft/awakening/mixin/block/MixinAbstractFluidBlock.java
+++ src/main/java/dev/adventurecraft/awakening/mixin/block/MixinAbstractFluidBlock.java
@@ -1,12 +1,16 @@
 package dev.adventurecraft.awakening.mixin.block;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.adventurecraft.awakening.common.AC_DebugMode;
 import dev.adventurecraft.awakening.common.AC_TerrainImage;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelSource;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.tile.LiquidTile;
 import net.minecraft.world.level.tile.Tile;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LiquidTile.class)
 public abstract class MixinAbstractFluidBlock extends Tile {
@@ -31,4 +35,16 @@ public abstract class MixinAbstractFluidBlock extends Tile {
     public boolean mayPick() {
         return AC_DebugMode.active &&  AC_DebugMode.isFluidHittable;
     }
+
+    @Redirect(
+        method = "fizz",
+        at = @At(
+            value = "INVOKE",
+            target = "Ljava/lang/Math;random()D",
+            remap = false
+        ))
+    private double useFastRandomInFizz(@Local(argsOnly = true) Level level) {
+        return level.random.nextFloat();
+    }
+
 }
