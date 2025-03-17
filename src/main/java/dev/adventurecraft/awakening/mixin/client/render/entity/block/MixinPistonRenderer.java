787402d2cac7001f2fea27e53f38ad4f31cf2e23
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-17 12:25:39 +0100

    Update PistonRenderer mixin to improve stability

--- src/main/java/dev/adventurecraft/awakening/mixin/client/render/entity/block/MixinPistonRenderer.java
+++ src/main/java/dev/adventurecraft/awakening/mixin/client/render/entity/block/MixinPistonRenderer.java
@@ -4,24 +4,32 @@ import com.llamalad7.mixinextras.sugar.Local;
 import dev.adventurecraft.awakening.extension.block.ExBlock;
 import net.minecraft.client.renderer.tileentity.PistonRenderer;
 import net.minecraft.world.level.tile.Tile;
+import net.minecraft.world.level.tile.piston.PistonMovingTileEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(PistonRenderer.class)
 public abstract class MixinPistonRenderer {
 
-    @ModifyConstant(
+    @ModifyArg(
         method = "render(Lnet/minecraft/world/level/tile/piston/PistonMovingTileEntity;DDDF)V",
-        constant = @Constant(stringValue = "/terrain.png"))
-    private String useTerrainTexture(String constant, @Local Tile block) {
-        int texture = ((ExBlock) block).getTextureNum();
-        String var11;
-        if (texture == 0) {
-            var11 = constant;
-        } else {
-            var11 = String.format("/terrain%d.png", texture);
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/tileentity/PistonRenderer;bindTexture(Ljava/lang/String;)V"))
+    private String useTerrainTexture(String value, @Local(argsOnly = true) PistonMovingTileEntity entity) {
+        Tile tile = Tile.tiles[entity.getTileId()];
+        if (tile == null) {
+            return value;
         }
-        return var11;
+
+        int texture = ((ExBlock) tile).getTextureNum();
+        String path;
+        if (texture == 0) {
+            path = value;
+        } else {
+            path = String.format("/terrain%d.png", texture);
+        }
+        return path;
     }
 }
