509fb374389501125964db67e1408f9edeec1b16
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-22 22:03:26 +0100

    Cleanup GuiMessage

--- src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiMessage.java
+++ src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiMessage.java
@@ -6,22 +6,20 @@ import net.minecraft.SharedConstants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.world.level.Level;
+
 import java.util.ArrayList;
 
 public class AC_GuiMessage extends Screen {
 
     private AC_TileEntityMessage msg;
-    private Level world;
     private int page;
 
-    public AC_GuiMessage(Level world, AC_TileEntityMessage msg) {
-        this.world = world;
+    public AC_GuiMessage(AC_TileEntityMessage msg) {
         this.msg = msg;
     }
 
     public void init() {
-        ArrayList<String> soundList = ((ExWorld) this.world).getSoundList();
+        ArrayList<String> soundList = ((ExWorld) msg.level).getSoundList();
 
         int soundsPerPage = 3 * ((this.height - 60) / 20);
         for (int i = 0; i + soundsPerPage * this.page - 1 < soundList.size() && i < soundsPerPage; ++i) {
@@ -33,7 +31,7 @@ public class AC_GuiMessage extends Screen {
             int x = 4 + i % 3 * this.width / 3;
             int y = 60 + i / 3 * 20;
             int w = (this.width - 16) / 3;
-            
+
             var button = new Button(i, x, y, w, 18, name);
             this.buttons.add(button);
         }
@@ -47,45 +45,59 @@ public class AC_GuiMessage extends Screen {
 
     @Override
     protected void buttonClicked(Button button) {
+        String sound = this.msg.sound;
         if (button.id == 0 && this.page == 0) {
-            this.msg.sound = "";
+            sound = "";
         } else if (button.id < 100) {
             int soundId = 3 * ((this.height - 60) / 20);
-            this.msg.sound = ((ExWorld) this.world).getSoundList().get(button.id - 1 + soundId * this.page);
+            sound = ((ExWorld) msg.level).getSoundList().get(button.id - 1 + soundId * this.page);
         } else {
             this.page = button.id - 100;
             this.buttons.clear();
             this.init();
         }
 
-        this.world.getChunkAt(this.msg.x, this.msg.z).markUnsaved();
+        if (!this.msg.sound.equals(sound)) {
+            this.msg.sound = sound;
+            this.msg.setChanged();
+        }
     }
 
     @Override
-    protected void keyPressed(char var1, int var2) {
-        super.keyPressed(var1, var2);
-        if (var2 == 14 && this.msg.message.length() > 0) {
-            this.msg.message = this.msg.message.substring(0, this.msg.message.length() - 1);
+    protected void keyPressed(char ch, int key) {
+        super.keyPressed(ch, key);
+
+        String message = this.msg.message;
+        if (key == 14 && !message.isEmpty()) {
+            message = message.substring(0, message.length() - 1);
         }
 
-        if (SharedConstants.acceptableLetters.indexOf(var1) >= 0 && this.msg.message.length() < 30) {
-            this.msg.message = this.msg.message + var1;
+        if (SharedConstants.acceptableLetters.indexOf(ch) >= 0) {
+            message = message + ch;
+        }
+
+        if (!this.msg.message.equals(message)) {
+            this.msg.message = message;
+            this.msg.setChanged();
         }
     }
 
     public void render(int mouseX, int mouseY, float deltaTime) {
         this.renderBackground();
-        this.drawString(this.font, String.format("Message: '%s'", this.msg.message), 4, 4, 14737632);
+
+        int color = 14737632;
+        this.drawString(this.font, String.format("Message: '%s'", this.msg.message), 4, 4, color);
+
         if (!this.msg.sound.equals("")) {
-            this.drawString(this.font, String.format("Sound: %s", this.msg.sound), 4, 24, 14737632);
+            this.drawString(this.font, String.format("Sound: %s", this.msg.sound), 4, 24, color);
         } else {
-            this.drawString(this.font, "Sound: None", 4, 24, 14737632);
+            this.drawString(this.font, "Sound: None", 4, 24, color);
         }
 
         super.render(mouseX, mouseY, deltaTime);
     }
 
-    public static void showUI(Level var0, AC_TileEntityMessage var1) {
-        Minecraft.instance.setScreen(new AC_GuiMessage(var0, var1));
+    public static void showUI(AC_TileEntityMessage entity) {
+        Minecraft.instance.setScreen(new AC_GuiMessage(entity));
     }
 }
