c8116fe352b7ac07899deac4af8fa54f87c7e965
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-22 21:46:24 +0100

    Cleanup GuiTriggerInverter

--- src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiTriggerInverter.java
+++ src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiTriggerInverter.java
@@ -1,26 +1,18 @@
 package dev.adventurecraft.awakening.common.gui;
 
 import dev.adventurecraft.awakening.tile.AC_Blocks;
+import dev.adventurecraft.awakening.tile.entity.AC_TileEntityMinMax;
 import dev.adventurecraft.awakening.tile.entity.AC_TileEntityTriggerInverter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.OptionButton;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.world.level.Level;
 
 public class AC_GuiTriggerInverter extends Screen {
     private AC_TileEntityTriggerInverter trigger;
-    private int blockX;
-    private int blockY;
-    private int blockZ;
-    private Level world;
 
-    public AC_GuiTriggerInverter(Level var1, int var2, int var3, int var4, AC_TileEntityTriggerInverter var5) {
-        this.world = var1;
-        this.blockX = var2;
-        this.blockY = var3;
-        this.blockZ = var4;
-        this.trigger = var5;
+    public AC_GuiTriggerInverter(AC_TileEntityTriggerInverter entity) {
+        this.trigger = entity;
     }
 
     public void tick() {
@@ -30,24 +22,28 @@ public class AC_GuiTriggerInverter extends Screen {
         this.buttons.add(new OptionButton(0, 4, 40, "Use Current Selection"));
     }
 
-    protected void buttonClicked(Button var1) {
-        int var2 = this.world.getTile(this.blockX, this.blockY, this.blockZ);
-        if (var2 == AC_Blocks.triggerInverter.id) {
-            AC_Blocks.triggerInverter.setTriggerToSelection(this.world, this.blockX, this.blockY, this.blockZ);
+    protected void buttonClicked(Button btn) {
+        AC_TileEntityTriggerInverter t = this.trigger;
+        int id = t.level.getTile(t.x, t.y, t.z);
+        if (id == AC_Blocks.triggerInverter.id) {
+            AC_Blocks.triggerInverter.setTriggerToSelection(t.level, t.x, t.y, t.z);
+            t.setChanged();
         }
-
-        this.world.getChunkAt(this.blockX, this.blockZ).markUnsaved();
     }
 
-    public void render(int var1, int var2, float var3) {
+    public void render(int mouseX, int mouseY, float tick) {
         this.fill(0, 0, this.width, this.height, Integer.MIN_VALUE);
-        this.drawString(this.font, String.format("Min: (%d, %d, %d)", this.trigger.minX, this.trigger.minY, this.trigger.minZ), 4, 4, 14737632);
-        this.drawString(this.font, String.format("Max: (%d, %d, %d)", this.trigger.maxX, this.trigger.maxY, this.trigger.maxZ), 4, 24, 14737632);
-        super.render(var1, var2, var3);
+
+        AC_TileEntityMinMax t = this.trigger;
+        int color = 14737632;
+        this.drawString(this.font, String.format("Min: (%d, %d, %d)", t.minX, t.minY, t.minZ), 4, 4, color);
+        this.drawString(this.font, String.format("Max: (%d, %d, %d)", t.maxX, t.maxY, t.maxZ), 4, 24, color);
+
+        super.render(mouseX, mouseY, tick);
     }
 
-    public static void showUI(Level var0, int var1, int var2, int var3, AC_TileEntityTriggerInverter var4) {
-        Minecraft.instance.setScreen(new AC_GuiTriggerInverter(var0, var1, var2, var3, var4));
+    public static void showUI(AC_TileEntityTriggerInverter entity) {
+        Minecraft.instance.setScreen(new AC_GuiTriggerInverter(entity));
     }
 
     public boolean isPauseScreen() {
