bcf8783cd2e09a289ad5c5ee157c008f2227bd6c
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-22 21:31:09 +0100

    Cleanup GuiRedstoneTrigger

--- src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiRedstoneTrigger.java
+++ src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiRedstoneTrigger.java
@@ -10,17 +10,9 @@ import net.minecraft.world.level.Level;
 
 public class AC_GuiRedstoneTrigger extends Screen {
     private AC_TileEntityRedstoneTrigger trigger;
-    private int blockX;
-    private int blockY;
-    private int blockZ;
-    private Level world;
 
-    public AC_GuiRedstoneTrigger(Level var1, int var2, int var3, int var4, AC_TileEntityRedstoneTrigger var5) {
-        this.world = var1;
-        this.blockX = var2;
-        this.blockY = var3;
-        this.blockZ = var4;
-        this.trigger = var5;
+    public AC_GuiRedstoneTrigger(AC_TileEntityRedstoneTrigger entity) {
+        this.trigger = entity;
     }
 
     public void tick() {
@@ -28,41 +20,47 @@ public class AC_GuiRedstoneTrigger extends Screen {
 
     public void init() {
         this.buttons.add(new OptionButton(0, 4, 40, "Use Current Selection"));
-        OptionButton var1 = new OptionButton(1, 4, 60, "Trigger Target");
+
+        var var1 = new OptionButton(1, 4, 60, "Trigger Target");
         if (this.trigger.resetOnTrigger) {
             var1.message = "Reset Target";
         }
-
         this.buttons.add(var1);
     }
 
-    protected void buttonClicked(Button var1) {
-        if (var1.id == 0) {
-            int var2 = this.world.getTile(this.blockX, this.blockY, this.blockZ);
-            if (var2 == AC_Blocks.redstoneTrigger.id) {
-                AC_Blocks.redstoneTrigger.setTriggerToSelection(this.world, this.blockX, this.blockY, this.blockZ);
+    protected void buttonClicked(Button btn) {
+        AC_TileEntityRedstoneTrigger trigger = this.trigger;
+        if (btn.id == 0) {
+            int id = trigger.level.getTile(trigger.x, trigger.y, trigger.z);
+            if (id == AC_Blocks.redstoneTrigger.id) {
+                AC_Blocks.redstoneTrigger.setTriggerToSelection(trigger.level, trigger.x, trigger.y, trigger.z);
             }
-        } else if (var1.id == 1) {
+        } else if (btn.id == 1) {
             this.trigger.resetOnTrigger = !this.trigger.resetOnTrigger;
             if (this.trigger.resetOnTrigger) {
-                var1.message = "Reset Target";
+                btn.message = "Reset Target";
             } else {
-                var1.message = "Trigger Target";
+                btn.message = "Trigger Target";
             }
         }
 
-        this.world.getChunkAt(this.blockX, this.blockZ).markUnsaved();
+        this.trigger.setChanged();
     }
 
-    public void render(int var1, int var2, float var3) {
+    public void render(int mouseX, int mouseY, float tick) {
         this.fill(0, 0, this.width, this.height, Integer.MIN_VALUE);
-        this.drawString(this.font, String.format("Min: (%d, %d, %d)", this.trigger.minX, this.trigger.minY, this.trigger.minZ), 4, 4, 14737632);
-        this.drawString(this.font, String.format("Max: (%d, %d, %d)", this.trigger.maxX, this.trigger.maxY, this.trigger.maxZ), 4, 24, 14737632);
-        super.render(var1, var2, var3);
+
+        AC_TileEntityRedstoneTrigger t = this.trigger;
+        int color = 14737632;
+
+        this.drawString(this.font, String.format("Min: (%d, %d, %d)", t.minX, t.minY, t.minZ), 4, 4, color);
+        this.drawString(this.font, String.format("Max: (%d, %d, %d)", t.maxX, t.maxY, t.maxZ), 4, 24, color);
+
+        super.render(mouseX, mouseY, tick);
     }
 
-    public static void showUI(Level var0, int var1, int var2, int var3, AC_TileEntityRedstoneTrigger var4) {
-        Minecraft.instance.setScreen(new AC_GuiRedstoneTrigger(var0, var1, var2, var3, var4));
+    public static void showUI(AC_TileEntityRedstoneTrigger entity) {
+        Minecraft.instance.setScreen(new AC_GuiRedstoneTrigger(entity));
     }
 
     public boolean isPauseScreen() {
