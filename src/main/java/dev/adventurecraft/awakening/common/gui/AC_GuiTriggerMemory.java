291cc3da99cfcc24949b83ccac3b8af58d82b406
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-22 21:58:39 +0100

    Cleanup GuiTriggerMemory

--- src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiTriggerMemory.java
+++ src/main/java/dev/adventurecraft/awakening/common/gui/AC_GuiTriggerMemory.java
@@ -6,21 +6,12 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.OptionButton;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.world.level.Level;
 
 public class AC_GuiTriggerMemory extends Screen {
     private AC_TileEntityTriggerMemory trigger;
-    private int blockX;
-    private int blockY;
-    private int blockZ;
-    private Level world;
 
-    public AC_GuiTriggerMemory(Level var1, int var2, int var3, int var4, AC_TileEntityTriggerMemory var5) {
-        this.world = var1;
-        this.blockX = var2;
-        this.blockY = var3;
-        this.blockZ = var4;
-        this.trigger = var5;
+    public AC_GuiTriggerMemory(AC_TileEntityTriggerMemory entity) {
+        this.trigger = entity;
     }
 
     public void tick() {
@@ -28,60 +19,65 @@ public class AC_GuiTriggerMemory extends Screen {
 
     public void init() {
         this.buttons.add(new OptionButton(0, 4, 40, "Use Current Selection"));
-        OptionButton var1 = new OptionButton(1, 4, 60, "Activate on Trigger");
+
+        var var1 = new OptionButton(1, 4, 60, "Activate on Trigger");
         if (this.trigger.activateOnDetrigger) {
             var1.message = "Activate on Detrigger";
         }
-
         this.buttons.add(var1);
+
         var1 = new OptionButton(2, 4, 80, "Reset on Death");
         if (!this.trigger.resetOnDeath) {
             var1.message = "Don\'t Reset on Death";
         }
-
         this.buttons.add(var1);
     }
 
-    protected void buttonClicked(Button var1) {
-        if (var1.id == 0) {
-            int var2 = this.world.getTile(this.blockX, this.blockY, this.blockZ);
+    protected void buttonClicked(Button btn) {
+        AC_TileEntityTriggerMemory t = this.trigger;
+        if (btn.id == 0) {
+            int var2 = t.level.getTile(t.x, t.y, t.z);
             if (var2 == AC_Blocks.triggerMemory.id) {
-                AC_Blocks.triggerMemory.setTriggerToSelection(this.world, this.blockX, this.blockY, this.blockZ);
+                AC_Blocks.triggerMemory.setTriggerToSelection(t.level, t.x, t.y, t.z);
             }
-        } else if (var1.id == 1) {
-            this.trigger.activateOnDetrigger = !this.trigger.activateOnDetrigger;
-            if (this.trigger.activateOnDetrigger) {
-                var1.message = "Activate on Detrigger";
+        } else if (btn.id == 1) {
+            t.activateOnDetrigger = !t.activateOnDetrigger;
+            if (t.activateOnDetrigger) {
+                btn.message = "Activate on Detrigger";
             } else {
-                var1.message = "Activate on Trigger";
+                btn.message = "Activate on Trigger";
             }
-        } else if (var1.id == 2) {
-            this.trigger.resetOnDeath = !this.trigger.resetOnDeath;
-            if (this.trigger.resetOnDeath) {
-                var1.message = "Reset on Death";
+        } else if (btn.id == 2) {
+            t.resetOnDeath = !t.resetOnDeath;
+            if (t.resetOnDeath) {
+                btn.message = "Reset on Death";
             } else {
-                var1.message = "Don\'t Reset on Death";
+                btn.message = "Don't Reset on Death";
             }
         }
 
-        this.world.getChunkAt(this.blockX, this.blockZ).markUnsaved();
+        t.setChanged();
     }
 
-    public void render(int var1, int var2, float var3) {
+    public void render(int mouseX, int mouseY, float tick) {
         this.fill(0, 0, this.width, this.height, Integer.MIN_VALUE);
-        this.drawString(this.font, String.format("Min: (%d, %d, %d)", this.trigger.minX, this.trigger.minY, this.trigger.minZ), 4, 4, 14737632);
-        this.drawString(this.font, String.format("Max: (%d, %d, %d)", this.trigger.maxX, this.trigger.maxY, this.trigger.maxZ), 4, 24, 14737632);
-        if (this.trigger.isActivated) {
-            this.drawString(this.font, "Memory Set", 4, 104, 14737632);
+
+        AC_TileEntityTriggerMemory t = this.trigger;
+        int color = 14737632;
+
+        this.drawString(this.font, String.format("Min: (%d, %d, %d)", t.minX, t.minY, t.minZ), 4, 4, color);
+        this.drawString(this.font, String.format("Max: (%d, %d, %d)", t.maxX, t.maxY, t.maxZ), 4, 24, color);
+        if (t.isActivated) {
+            this.drawString(this.font, "Memory Set", 4, 104, color);
         } else {
-            this.drawString(this.font, "Memory Unset", 4, 104, 14737632);
+            this.drawString(this.font, "Memory Unset", 4, 104, color);
         }
 
-        super.render(var1, var2, var3);
+        super.render(mouseX, mouseY, tick);
     }
 
-    public static void showUI(Level var0, int var1, int var2, int var3, AC_TileEntityTriggerMemory var4) {
-        Minecraft.instance.setScreen(new AC_GuiTriggerMemory(var0, var1, var2, var3, var4));
+    public static void showUI(AC_TileEntityTriggerMemory entity) {
+        Minecraft.instance.setScreen(new AC_GuiTriggerMemory(entity));
     }
 
     public boolean isPauseScreen() {
