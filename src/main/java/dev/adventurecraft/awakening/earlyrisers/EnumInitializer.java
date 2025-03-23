4ffa8e94904266ad618e44a1a7387a9a7f1a3047
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-23 13:50:27 +0100

    Disable ImageIO disk cache

--- src/main/java/dev/adventurecraft/awakening/earlyrisers/EnumInitializer.java
+++ src/main/java/dev/adventurecraft/awakening/earlyrisers/EnumInitializer.java
@@ -24,48 +24,52 @@ public class EnumInitializer implements Runnable {
     }
 
     private void addOptifineOptions(String type) {
-        addOptionEnum(type, "FOG_FANCY", "options.of.detail.fogType", false, false);
-        addOptionEnum(type, "FOG_START", "options.of.detail.fogStart", false, false);
-        addOptionEnum(type, "MIPMAP_LEVEL", "options.of.texture.mipLevel", false, false);
-        addOptionEnum(type, "MIPMAP_TYPE", "options.of.texture.mipType", false, false);
-        addOptionEnum(type, "LOAD_FAR", "options.of.world.loadFar", false, false);
-        addOptionEnum(type, "PRELOADED_CHUNKS", "options.of.world.preloadedChunks", false, false);
-        addOptionEnum(type, "SMOOTH_FPS", "options.of.other.smoothFPS", false, false);
+        addOptionEnum(type, "FOG_FANCY", "options.of.detail.fogType");
+        addOptionEnum(type, "FOG_START", "options.of.detail.fogStart");
+        addOptionEnum(type, "MIPMAP_LEVEL", "options.of.texture.mipLevel");
+        addOptionEnum(type, "MIPMAP_TYPE", "options.of.texture.mipType");
+        addOptionEnum(type, "LOAD_FAR", "options.of.world.loadFar");
+        addOptionEnum(type, "PRELOADED_CHUNKS", "options.of.world.preloadedChunks");
+        addOptionEnum(type, "SMOOTH_FPS", "options.of.other.smoothFPS");
         addOptionEnum(type, "BRIGHTNESS", "options.of.brightness", true, false);
-        addOptionEnum(type, "CLOUDS", "options.of.detail.clouds", false, false);
+        addOptionEnum(type, "CLOUDS", "options.of.detail.clouds");
         addOptionEnum(type, "CLOUD_HEIGHT", "options.of.detail.cloudHeight", true, false);
-        addOptionEnum(type, "LEAVES", "options.of.texture.leaves", false, false);
-        addOptionEnum(type, "GRASS", "options.of.detail.grass", false, false);
-        addOptionEnum(type, "RAIN", "options.of.detail.rainAndSnow", false, false);
-        addOptionEnum(type, "WATER", "options.of.detail.water", false, false);
-        addOptionEnum(type, "ANIMATED_WATER", "options.of.texture.animated.water", false, false);
-        addOptionEnum(type, "ANIMATED_LAVA", "options.of.texture.animated.lava", false, false);
-        addOptionEnum(type, "ANIMATED_FIRE", "options.of.texture.animated.fire", false, false);
-        addOptionEnum(type, "ANIMATED_PORTAL", "options.of.texture.animated.portal", false, false);
+        addOptionEnum(type, "LEAVES", "options.of.texture.leaves");
+        addOptionEnum(type, "GRASS", "options.of.detail.grass");
+        addOptionEnum(type, "RAIN", "options.of.detail.rainAndSnow");
+        addOptionEnum(type, "WATER", "options.of.detail.water");
+        addOptionEnum(type, "ANIMATED_WATER", "options.of.texture.animated.water");
+        addOptionEnum(type, "ANIMATED_LAVA", "options.of.texture.animated.lava");
+        addOptionEnum(type, "ANIMATED_FIRE", "options.of.texture.animated.fire");
+        addOptionEnum(type, "ANIMATED_PORTAL", "options.of.texture.animated.portal");
         addOptionEnum(type, "AO_LEVEL", "options.ao", true, false);
-        addOptionEnum(type, "FAST_DEBUG_INFO", "options.of.other.fastDebugInfo", false, false);
-        addOptionEnum(type, "AUTOSAVE_TICKS", "options.of.world.autosaveInterval", false, false);
-        addOptionEnum(type, "CONNECTED_GRASS", "options.of.detail.connectedGrass", false, false);
-        addOptionEnum(type, "ANIMATED_REDSTONE", "options.of.texture.animated.redstone", false, false);
-        addOptionEnum(type, "ANIMATED_EXPLOSION", "options.of.texture.animated.explosion", false, false);
-        addOptionEnum(type, "ANIMATED_FLAME", "options.of.texture.animated.flame", false, false);
-        addOptionEnum(type, "ANIMATED_SMOKE", "options.of.texture.animated.smoke", false, false);
-        addOptionEnum(type, "WEATHER", "options.of.world.weather", false, false);
-        addOptionEnum(type, "SKY", "options.of.detail.sky", false, false);
-        addOptionEnum(type, "STARS", "options.of.detail.stars", false, false);
-        addOptionEnum(type, "FAR_VIEW", "options.of.world.farView", false, false);
-        addOptionEnum(type, "CHUNK_UPDATES", "options.of.world.chunkUpdates", false, false);
-        addOptionEnum(type, "CHUNK_UPDATES_DYNAMIC", "options.of.world.dynamicUpdates", false, false);
-        addOptionEnum(type, "TIME", "options.of.world.time", false, false);
-        addOptionEnum(type, "CLEAR_WATER", "options.of.detail.clearWater", false, false);
-        addOptionEnum(type, "SMOOTH_INPUT", "options.of.other.smoothInput", false, false);
-        addOptionEnum(type, "AA_LEVEL", "options.of.msaa", false, false);
-        addOptionEnum(type, "AF_LEVEL", "options.of.texture.anisoFilter", false, false);
+        addOptionEnum(type, "FAST_DEBUG_INFO", "options.of.other.fastDebugInfo");
+        addOptionEnum(type, "AUTOSAVE_TICKS", "options.of.world.autosaveInterval");
+        addOptionEnum(type, "CONNECTED_GRASS", "options.of.detail.connectedGrass");
+        addOptionEnum(type, "ANIMATED_REDSTONE", "options.of.texture.animated.redstone");
+        addOptionEnum(type, "ANIMATED_EXPLOSION", "options.of.texture.animated.explosion");
+        addOptionEnum(type, "ANIMATED_FLAME", "options.of.texture.animated.flame");
+        addOptionEnum(type, "ANIMATED_SMOKE", "options.of.texture.animated.smoke");
+        addOptionEnum(type, "WEATHER", "options.of.world.weather");
+        addOptionEnum(type, "SKY", "options.of.detail.sky");
+        addOptionEnum(type, "STARS", "options.of.detail.stars");
+        addOptionEnum(type, "FAR_VIEW", "options.of.world.farView");
+        addOptionEnum(type, "CHUNK_UPDATES", "options.of.world.chunkUpdates");
+        addOptionEnum(type, "CHUNK_UPDATES_DYNAMIC", "options.of.world.dynamicUpdates");
+        addOptionEnum(type, "TIME", "options.of.world.time");
+        addOptionEnum(type, "CLEAR_WATER", "options.of.detail.clearWater");
+        addOptionEnum(type, "SMOOTH_INPUT", "options.of.other.smoothInput");
+        addOptionEnum(type, "AA_LEVEL", "options.of.msaa");
+        addOptionEnum(type, "AF_LEVEL", "options.of.texture.anisoFilter");
     }
 
     private static void addOptionEnum(String type, String name, String translationKey, boolean slider, boolean toggle) {
         ClassTinkerers.enumBuilder(type, String.class, boolean.class, boolean.class)
             .addEnum(name, translationKey, slider, toggle)
             .build();
+    }
+
+    private static void addOptionEnum(String type, String name, String translationKey) {
+        addOptionEnum(type, name, translationKey, false, false);
     }
 }
