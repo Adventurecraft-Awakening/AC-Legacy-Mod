e2d8b1f7a4128e661d3e78632a7288052c8f518e
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-05-04 15:27:32 +0200

    Add Optional<T> APIs to CompoundTag

--- src/main/java/dev/adventurecraft/awakening/extension/util/io/ExCompoundTag.java
+++ src/main/java/dev/adventurecraft/awakening/extension/util/io/ExCompoundTag.java
@@ -1,10 +1,43 @@
 package dev.adventurecraft.awakening.extension.util.io;
 
+import net.minecraft.nbt.*;
+
+import java.util.Optional;
 import java.util.Set;
 
 public interface ExCompoundTag {
 
+    default Optional<Boolean> findBool(String key) {
+        return this.findByte(key).map(b -> b != 0);
+    }
+
+    default Optional<Float> findFloat(String key) {
+        return this.findTag(key).map(tag -> tag instanceof FloatTag dTag ? dTag.data : null);
+    }
+
+    default Optional<Double> findDouble(String key) {
+        return this.findTag(key).map(tag -> tag instanceof DoubleTag dTag ? dTag.data : null);
+    }
+
+    Optional<Byte> findByte(String key);
+
+    Optional<Short> findShort(String key);
+
+    Optional<Integer> findInt(String key);
+
+    Optional<Long> findLong(String key);
+
+    default Optional<String> findString(String key) {
+        return this.findTag(key).map(tag -> tag instanceof StringTag sTag ? sTag.contents : null);
+    }
+
+    default Optional<CompoundTag> findCompound(String key) {
+        return this.findTag(key).map(tag -> tag instanceof CompoundTag cTag ? cTag : null);
+    }
+
     Set<String> getKeys();
 
-    Object getValue(String key);
+    Tag getTag(String key);
+
+    Optional<Tag> findTag(String key);
 }
