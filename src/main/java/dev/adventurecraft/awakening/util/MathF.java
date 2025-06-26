de2ada510ccf3104aed2f3d8e628619262e52557
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-06-26 18:22:49 +0200

    Micro-opt AoHelper

--- src/main/java/dev/adventurecraft/awakening/util/MathF.java
+++ src/main/java/dev/adventurecraft/awakening/util/MathF.java
@@ -12,12 +12,20 @@ public final class MathF {
         return (int) Math.min(max, Math.max(value, min));
     }
 
+    public static float clamp(float value, float min, float max) {
+        return Math.min(max, Math.max(value, min));
+    }
+
     public static float cubicInterpolation(float mu, float y0, float y1, float y2, float y3) {
         float mu2 = mu * mu;
         float a0 = -0.5F * y0 + 1.5F * y1 - 1.5F * y2 + 0.5F * y3;
         float a1 = y0 - 2.5F * y1 + 2.0F * y2 - 0.5F * y3;
         float a2 = -0.5F * y0 + 0.5F * y2;
         return a0 * mu * mu2 + a1 * mu2 + a2 * mu + y1;
+    }
+
+    public static float fastLerp(float amount, float start, float end) {
+        return (end - start) * amount + start;
     }
 
     public static float lerp(float amount, float start, float end) {
