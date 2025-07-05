c958616f69c1637e8182f21ab543f8b922ca633a
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-07-05 21:43:14 +0200

    Fix PR merge issues

--- src/main/java/dev/adventurecraft/awakening/mixin/world/MixinCakeTile.java
+++ src/main/java/dev/adventurecraft/awakening/mixin/world/MixinCakeTile.java
@@ -1,27 +1,21 @@
 package dev.adventurecraft.awakening.mixin.world;
 
-import dev.adventurecraft.awakening.extension.entity.ExLivingEntity;
+import com.llamalad7.mixinextras.sugar.Local;
+import dev.adventurecraft.awakening.extension.entity.ExMob;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.tile.CakeTile;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(CakeTile.class)
 public abstract class MixinCakeTile {
 
-    @Overwrite
-    private void eat(Level level, int x, int y, int z, Player player) {
-        if (player.health < ((ExLivingEntity)player).getMaxHealth()) {
-            player.heal(3);
-            int var6 = level.getData(x, y, z) + 1;
-            if (var6 >= 6) {
-                level.setTile(x, y, z, 0);
-            } else {
-                level.setData(x, y, z, var6);
-                level.setTileDirty(x, y, z);
-            }
-        }
-
+    @ModifyConstant(
+        method = "eat",
+        constant = @Constant(intValue = 20)
+    )
+    private int respectMaxHealthWhenEating(int constant, @Local(argsOnly = true) Player player) {
+        return ((ExMob) player).getMaxHealth();
     }
 }
