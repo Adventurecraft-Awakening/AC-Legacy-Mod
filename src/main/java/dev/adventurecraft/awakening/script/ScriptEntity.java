17ceb39c20c36afc3df554ac2b4fe2daa709edb8
Hormez2 <jonas.hormess@web.de>
2025-08-30 16:51:51 +0200

    - changed nested if statements to a switch statement

--- src/main/java/dev/adventurecraft/awakening/script/ScriptEntity.java
+++ src/main/java/dev/adventurecraft/awakening/script/ScriptEntity.java
@@ -35,20 +35,22 @@ public class ScriptEntity {
     }
 
     public static ScriptEntity getEntityClass(Entity entity) {
-        if (entity == null) return null;
-        if (entity instanceof Player e) return new ScriptEntityPlayer(e);
-        if (entity instanceof Monster e) return new ScriptEntityMob(e);
-        if (entity instanceof Wolf e) return new ScriptEntityWolf(e);
-        if (entity instanceof PathfinderMob e) return new ScriptEntityCreature(e);
-        if (entity instanceof FlyingMob e) return new ScriptEntityFlying(e);
-        if (entity instanceof AC_EntityNPC e) return new ScriptEntityNPC(e);
-        if (entity instanceof AC_EntityLivingScript e) return new ScriptEntityLivingScript(e);
-        if (entity instanceof Slime e) return new ScriptEntitySlime(e);
-        if (entity instanceof Mob e) return new ScriptEntityLiving(e);
-        if (entity instanceof Arrow e) return new ScriptEntityArrow(e);
-        if (entity instanceof ItemEntity e) return new ScriptEntityItem(e);
-        if (entity instanceof AC_Particle e) return new ScriptParticleEntity(e);
-        return new ScriptEntity(entity);
+        return switch (entity) {
+            case null -> null;
+            case Player e -> new ScriptEntityPlayer(e);
+            case Monster e -> new ScriptEntityMob(e);
+            case Wolf e -> new ScriptEntityWolf(e);
+            case PathfinderMob e -> new ScriptEntityCreature(e);
+            case FlyingMob e -> new ScriptEntityFlying(e);
+            case AC_EntityNPC e -> new ScriptEntityNPC(e);
+            case AC_EntityLivingScript e -> new ScriptEntityLivingScript(e);
+            case Slime e -> new ScriptEntitySlime(e);
+            case Mob e -> new ScriptEntityLiving(e);
+            case Arrow e -> new ScriptEntityArrow(e);
+            case ItemEntity e -> new ScriptEntityItem(e);
+            case AC_Particle e -> new ScriptParticleEntity(e);
+            default -> new ScriptEntity(entity);
+        };
     }
 
     public void setCanGetFallDamage(boolean arg) {
