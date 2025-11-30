50b59ce7d72ceefb3969debb6bf0413f00c9b9bd
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-11-30 01:23:02 +0100

    Expose ExPainting.getTilePos

--- src/main/java/dev/adventurecraft/awakening/mixin/entity/decoration/painting/MixinPaintingEntity.java
+++ src/main/java/dev/adventurecraft/awakening/mixin/entity/decoration/painting/MixinPaintingEntity.java
@@ -1,15 +1,22 @@
 package dev.adventurecraft.awakening.mixin.entity.decoration.painting;
 
 import dev.adventurecraft.awakening.common.AC_DebugMode;
+import dev.adventurecraft.awakening.common.Coord;
+import dev.adventurecraft.awakening.extension.world.entity.item.ExPainting;
 import net.minecraft.world.entity.item.Painting;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Painting.class)
-public abstract class MixinPaintingEntity {
+public abstract class MixinPaintingEntity implements ExPainting {
+
+    @Shadow public int xTile;
+    @Shadow public int yTile;
+    @Shadow public int zTile;
 
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void disableTick(CallbackInfo ci) {
@@ -22,5 +29,10 @@ public abstract class MixinPaintingEntity {
         if (!AC_DebugMode.active) {
             ci.setReturnValue(false);
         }
+    }
+
+    @Override
+    public Coord getTilePos() {
+        return new Coord(this.xTile, this.yTile, this.zTile);
     }
 }
