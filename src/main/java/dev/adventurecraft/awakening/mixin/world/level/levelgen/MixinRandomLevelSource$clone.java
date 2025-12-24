46b4e54b679514d4c9f2060dbe5b55f700d8b998
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-12-24 22:04:18 +0100

    Clone BiomeSource in MixinRandomLevelSource$clone

--- src/main/java/dev/adventurecraft/awakening/mixin/world/level/levelgen/MixinRandomLevelSource$clone.java
+++ src/main/java/dev/adventurecraft/awakening/mixin/world/level/levelgen/MixinRandomLevelSource$clone.java
@@ -1,17 +1,22 @@
 package dev.adventurecraft.awakening.mixin.world.level.levelgen;
 
 import dev.adventurecraft.awakening.ACMod;
+import dev.adventurecraft.awakening.extension.world.level.biome.ExBiomeSource;
 import dev.adventurecraft.awakening.extension.world.level.worldgen.ExLargeFeature;
 import dev.adventurecraft.awakening.extension.world.level.worldgen.ExRandomLevelSource;
 import dev.adventurecraft.awakening.util.RandomUtil;
 import dev.adventurecraft.awakening.world.level.storage.AsyncChunkSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.levelgen.LargeFeature;
 import net.minecraft.world.level.levelgen.RandomLevelSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
@@ -21,6 +26,7 @@ import java.util.concurrent.CompletionStage;
 public abstract class MixinRandomLevelSource$clone implements Cloneable, ExRandomLevelSource, AsyncChunkSource {
 
     @Shadow private Random random;
+    @Shadow private Level level;
 
     @Shadow private double[] buffer;
     @Shadow private double[] sandBuffer;
@@ -34,11 +40,28 @@ public abstract class MixinRandomLevelSource$clone implements Cloneable, ExRando
     @Shadow double[] br;
     @Shadow double[] sr;
     @Shadow double[] dr;
-    @Shadow int[][] waterDepths;
     @Shadow private double[] temperatures;
+
+    @Unique private BiomeSource biomeSource;
 
     @Shadow
     public abstract LevelChunk loadChunk(int x, int z);
+
+    // Redirecting "postProcess" is not strictly necessary since it is called on main thread,
+    // but it should not hurt in this case.
+    @Redirect(
+        method = {"getChunk", "getHeights", "postProcess"},
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/Level;getBiomeSource()Lnet/minecraft/world/level/biome/BiomeSource;"
+        )
+    )
+    private BiomeSource useLocalBiomeSource(Level instance) {
+        if (this.biomeSource != null) {
+            return this.biomeSource;
+        }
+        return instance.getBiomeSource();
+    }
 
     @Override
     public void ac$initCopy() {
@@ -54,8 +77,10 @@ public abstract class MixinRandomLevelSource$clone implements Cloneable, ExRando
         this.br = null;
         this.sr = null;
         this.dr = null;
-        this.waterDepths = new int[32][32];
+        // this.waterDepths = new int[32][32]; // unused in vanilla
         this.temperatures = null;
+
+        this.biomeSource = ((ExBiomeSource) this.level.getBiomeSource()).copy();
     }
 
     @Override
@@ -73,7 +98,7 @@ public abstract class MixinRandomLevelSource$clone implements Cloneable, ExRando
 
     @Override
     public CompletionStage<LevelChunk> loadAsync(Level level, int x, int z) {
-        var self = this.ac$clone();
+        RandomLevelSource self = this.ac$clone();
         return CompletableFuture.supplyAsync(() -> self.getChunk(x, z), ACMod.WORLD_GEN_EXECUTOR);
     }
 }
